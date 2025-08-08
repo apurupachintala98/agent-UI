d70842a1e3b33ec1bc3b6b031b8d959a924387ef
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-08-08 20:35:00 +0530

    push

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,5 +1,9 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+console.log("API Base URL:", API_BASE_URL);
+
 export const sendToAgent = async (user_ques: string, session_id: string) => {
-  const res = await fetch(`${VITE_API_BASE_URL}`, {
+  const res = await fetch(`${API_BASE_URL}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -17,3 +21,4 @@ export const sendToAgent = async (user_ques: string, session_id: string) => {
   const data = await res.json();
   return data; 
 };
+
